967609ee79995dd16a408be9a5c3412606cf205b
Shridhar Poman <shridharpoman@gmail.com>
2019-12-10 11:16:55 +0530

    third

--- first.js
+++ first.js
@@ -1,3 +1,4 @@
 Populate the welcome text
-var name = "Hello Customer";
-$("#welcome").text
+var is_new = false;
+var name = (is_new ? "Hello New Customer" : "Hello Customer")
+$("#welcome").text(name);
